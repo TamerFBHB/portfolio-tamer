c76f5c9fcdbd8a46f5aa2cff0d08463d179814e5
Tamer Fouad <tamer.fbhb.wd@gmail.com>
2024-01-05 01:45:29 +0200

    edit in navbar

--- src/000-MobileBar/MobileBar.jsx
+++ src/000-MobileBar/MobileBar.jsx
@@ -1,3 +1,4 @@
+import { Textillate } from "textillate-react";
 import LogoT from "../Component/LogoT/LogoT";
 import "./MobileBar.scss";
 import { IoClose } from "react-icons/io5";
@@ -6,6 +7,34 @@ const MobileBar = ({show,setShow}) => {
     return (
         <div className="mobilebar">
             <LogoT />
+
+            <div className="myName">
+                        <p>
+                            <Textillate
+                                option={{
+                                    in: {
+                                        effect: "fadeInLeftBig",
+                                        delay: 20,
+                                        delayScale: 3,
+                                        sequence: true,
+                                        loop: false,
+                                    },
+                                    out: {
+                                        effect: "fadeOutUp",
+                                        sequence: true,
+                                        delay: 20,
+                                        delayScale: 3,
+                                        loop: false,
+                                    },
+                                    type: "char",
+                                    loop: true,
+                                }}
+                            >
+                                Tamer Fouad Boshara
+                            </Textillate>
+                        </p>
+                    </div>
+
             <div className="menuSideBar " 
             onClick={()=>{show?setShow(false):setShow(true)}}>
                 <span className={`close ${show? "none" : null }`}></span> 
